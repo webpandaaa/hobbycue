37eb84fd81303e7179ef3332b7c80e45e02acdef
webpandaaa <ankushrajsingh2004@gmail.com>
2024-07-06 23:42:39 +0530

    first

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='nav'>
+      <div className="navleft">
+        <img src="./Logo.png" alt="" />
+        <div className="searchdiv">
+            <input type="text" placeholder='Search here...' />
+            <i class="ri-search-line"></i>
+        </div>
+      </div>
+      <div className="navright">
+        <div className='firstpart'>
+          <img src="./explore.png" alt="" />
+          <img src="./hobbies.png" alt="" />
+        </div>
+        <div className="secondpart">
+          <i class="ri-bookmark-fill"></i>
+          <i class="ri-notification-4-fill"></i>
+          <i class="ri-shopping-cart-2-fill"></i>
+          <button>Sign in</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Navbar from '../components/Navbar/Navbar'
+import Hero from '../components/Hero/Hero'
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+    </div>
+  )
+}
+
+export default Home
